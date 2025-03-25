d5f7b23c17a5d1b223855558d4dbce35d5203636
Kemal <70811837+dev-Kemal@users.noreply.github.com>
2025-03-25 01:32:33 +0100

    Add Hangman project

---
+++ Hangman - UFO Edition/ufo.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main()
+{
+    std::string codeword = "codecademy";
+    std::string answer = "__________";
+    int misses = 0;
+    std::vector<char> incorrect;
+    bool guess = false;
+
+    greet();
+
+    char letter;
+    while (answer != codeword && misses < 7)
+    {
+        display_misses(misses);
+        display_status(incorrect, answer);
+
+        std::cout << "\nPlease enter your guess:";
+        std::cin >> letter;
+
+        for (int i = 0; i < codeword.length(); i++)
+        {
+            if (letter == codeword[i])
+            {
+                answer[i] = letter;
+                guess = true;
+            }
+        }
+
+        if (guess)
+        {
+            std::cout << "\nCorrect!\n";
+        }
+        else
+        {
+            std::cout << "\nIncorrect! The tractor beam pulls the person further.\n";
+
+            incorrect.push_back(letter);
+            misses++;
+        }
+
+        guess = false;
+    }
+
+    end_game(codeword, answer);
+}
